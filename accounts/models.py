7a192e9dc7d349c749356dd0422b9886caa4a42f
Illia Gusiev <illagusev078@gmail.com>
2026-07-09 20:25:17 +0300

    wip: створено базовий клас User з вибором ролей

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models 
 
-# Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        STUDENT = 'student', 'Учень'
+        MODERATOR = 'moderator', 'Модератор'
+        ADMIN =  'admin', 'Адміністратор'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.STUDENT,
+        verbose_name="Роль"
+    )
+
+#TODO: дописати логіку
